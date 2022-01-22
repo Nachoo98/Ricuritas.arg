4dab5c22dc3cf59017a10ac849b9a6cd11df72ac
Jazmin Oballos <jazminoballos55@gmail.com>
2022-01-22 18:37:56 -0300

    header

--- app.js
+++ app.js
@@ -2,9 +2,16 @@ const express=require('express');
 const path=require('path');
 const app=express();
 
+const publicPath=path.resolve(__dirname,'./public');
+app.use(express.static(publicPath))
+
 
 app.get('/',(req,res)=>{
     res.send("Bienvenidos");
+})
+
+app.get('/home',(req,res)=>{
+    res.sendFile(path.join(__dirname,'views/home.html'));
 })
 
 app.listen(3015,);
