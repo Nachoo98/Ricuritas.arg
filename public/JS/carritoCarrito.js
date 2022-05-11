39760bd6669bbcc10204523ca2e0a9d3602e83eb
Matias Sosa <matiasosa94@gmail.com>
2022-05-11 20:23:15 -0300

    carrito funcional

--- public/JS/carritoCarrito.js
+++ public/JS/carritoCarrito.js
@@ -3,7 +3,8 @@ window.addEventListener('load', function() {
     const contenedorTotal = document.getElementById('contenedorDeTotal');
     var productos = JSON.parse(localStorage.getItem('carrito'));
     var eliminar = document.getElementById('eliminar');
-    var activado = false;
+    var activadoVacio = false;
+    var activadoListas = true;
 
     function estadoCarrito() {
         let a
@@ -11,7 +12,7 @@ window.addEventListener('load', function() {
         return a
     }
 
-    if (estadoCarrito() == false) {
+    if (estadoCarrito() == false && activadoListas == true) {
         var total = 0
         productos.forEach(producto => {
             const div = document.createElement('div');
@@ -40,58 +41,64 @@ window.addEventListener('load', function() {
 
     }
 
-    if (productos.length === 0 && activado == false) {
+    if (productos.length === 0 && activadoVacio == false) {
         const div = document.createElement('div');
         div.classList.add('nacho_subproducto');
         div.innerHTML += `<div> <p>El carrito esta vacio</p></div>`
         contenedorProductos.appendChild(div);
-        activado = true
+        activadoVacio = true
     }
 
     eliminar.addEventListener('click', (e) => {
         if (productos.length != 0) {
             if (estadoCarrito() == false) {
+                activadoListas = false
                 let eliminado = productos.pop()
                 localStorage.setItem('carrito', JSON.stringify(productos))
-                if (estadoCarrito() == false) {
+                contenedorProductos.innerHTML = ''
+                contenedorTotal.innerHTML = ''
+                if (productos.length != 0) {
                     var total = 0
                     productos.forEach(producto => {
-                        contenedorProductos.removeChild(div)
-                        const div = document.createElement('div');
-                        div.classList.add('nacho_subproducto');
-                        div.innerHTML += `<div class="nacho_izquierda">
-                                        <img class="nacho_foto" src="imag/${producto.foto}">
-                                        <div class="nacho_detalle">
-                                        <h4>${producto.nombre}</h4>
-                                        <p> ${producto.descripcion}</p>
-                                        </div>
-                                    </div>
-                                    <div class="nacho_derecha">
-                                        
-                                        <div> Cantidad : ${producto.cantidad}</div>
-                                        <div> Precio : ${producto.precio}</div>
-                                        <div><b>Subtotal</b> $${producto.precio * producto.cantidad}</div>
-                                    </div>`
-                        contenedorProductos.appendChild(div);
+                        const div1 = document.createElement('div');
+                        div1.classList.add('nacho_subproducto');
+                        div1.innerHTML = `<div class="nacho_izquierda">
+                                                <img class="nacho_foto" src="imag/${producto.foto}">
+                                                <div class="nacho_detalle">
+                                                <h4>${producto.nombre}</h4>
+                                                <p> ${producto.descripcion}</p>
+                                                </div>
+                                            </div>
+                                            <div class="nacho_derecha">
+                                                
+                                                <div> Cantidad : ${producto.cantidad}</div>
+                                                <div> Precio : ${producto.precio}</div>
+                                                <div><b>Subtotal</b> $${producto.precio * producto.cantidad}</div>
+                                            </div>`
+                        contenedorProductos.appendChild(div1);
                         total = total + producto.precio * producto.cantidad
+
                     });
                     const dev = document.createElement('dev');
                     dev.classList.add('nacho_total');
-                    dev.innerHTML == `<p><b>TOTAL</b>: ${total}</p>`
-
+                    dev.innerHTML = `<p><b>TOTAL</b>: ${total}</p>`
                     contenedorTotal.appendChild(dev);
-
                 }
 
 
 
 
-                if (productos.length === 0 && activado == false) {
+                if (productos.length === 0 && activadoVacio == false) {
                     const div = document.createElement('div');
                     div.classList.add('nacho_subproducto');
-                    div.innerHTML += `<div> <p>El carrito esta vacio</p></div>`
+                    div.innerHTML = `<div> <p>El carrito esta vacio</p></div>`
                     contenedorProductos.appendChild(div);
-                    activado = true;
+                    contenedorTotal.innerHTML = '';
+                    const dev = document.createElement('div');
+                    dev.classList.add('nacho_total');
+                    dev.innerHTML = `<p><b>TOTAL</b>: $0</p>`
+                    contenedorTotal.appendChild(dev);
+                    activadoVacio = true;
                 }
             }
         }
